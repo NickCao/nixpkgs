84a8b2e81b2dfab52b16c8480911ae5dceba76f2
Nick Cao <nickcao@nichi.co>
2024-08-09 16:28:23 -0400

    [hack] gnome-gsettings-overrides

--- pkgs/desktops/gnome/nixos/gsettings-overrides/default.nix
+++ pkgs/desktops/gnome/nixos/gsettings-overrides/default.nix
@@ -39,7 +39,7 @@ let
 
 in
 
-runCommand "gnome-gsettings-overrides" { preferLocalBuild = true; } ''
+runCommand "gnome-gsettings-overrides" { preferLocalBuild = true; nativeBuildInputs = [ glib ]; } ''
   data_dir="$out/share/gsettings-schemas/nixos-gsettings-overrides"
   schema_dir="$data_dir/glib-2.0/schemas"
   mkdir -p "$schema_dir"
@@ -51,5 +51,5 @@ runCommand "gnome-gsettings-overrides" { preferLocalBuild = true; } ''
   ${gsettingsOverrides}
   EOF
 
-  ${glib.dev}/bin/glib-compile-schemas --strict "$schema_dir"
+  glib-compile-schemas --strict "$schema_dir"
 ''
