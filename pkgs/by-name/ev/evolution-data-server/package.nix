d1ec28ed1cb36f65d9ee76a6e01e2ae6d8e6da4b
Nick Cao <nickcao@nichi.co>
2024-09-28 09:55:42 -0400

    evolution-data-server: mark broken when cross compiling

--- pkgs/by-name/ev/evolution-data-server/package.nix
+++ pkgs/by-name/ev/evolution-data-server/package.nix
@@ -185,5 +185,6 @@ stdenv.mkDerivation rec {
     license = licenses.lgpl2Plus;
     maintainers = teams.gnome.members;
     platforms = platforms.unix;
+    broken = !stdenv.buildPlatform.canExecute stdenv.hostPlatform;
   };
 }
