95c282373d97fa838167addcd5138b2bb5d56fd6
Nick Cao <nickcao@nichi.co>
2025-02-11 17:02:19 -0500

    python312Packages.dscribe: fix build

--- pkgs/development/python-modules/dscribe/default.nix
+++ pkgs/development/python-modules/dscribe/default.nix
@@ -1,7 +1,8 @@
 {
-  buildPythonPackage,
   lib,
+  buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   numpy,
   scipy,
   ase,
@@ -14,18 +15,18 @@
 buildPythonPackage rec {
   pname = "dscribe";
   version = "2.1.1";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "singroup";
     repo = "dscribe";
-    rev = "v${version}";
+    tag = "v${version}";
     fetchSubmodules = true; # Bundles a specific version of Eigen
     hash = "sha256-2JY24cR2ie4+4svVWC4rm3Iy6Wfg0n2vkINz032kPWc=";
   };
 
-  pyproject = true;
-
   build-system = [
+    setuptools
     pybind11
   ];
 
@@ -33,15 +34,15 @@ buildPythonPackage rec {
     numpy
     scipy
     ase
+    scikit-learn
     joblib
     sparse
-    scikit-learn
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Machine learning descriptors for atomistic systems";
     homepage = "https://github.com/SINGROUP/dscribe";
-    license = licenses.asl20;
-    maintainers = [ maintainers.sheepforce ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ sheepforce ];
   };
 }
