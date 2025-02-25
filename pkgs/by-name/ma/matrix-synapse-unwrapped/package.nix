143710ad2441ad477b1c6d7ce9aa9fa71ac225b4
Sumner Evans <sumner.evans@automattic.com>
2025-02-25 10:39:27 -0700

    matrix-synapse: 1.124.0 -> 1.125
    https://github.com/element-hq/synapse/releases/tag/v1.125.0
    
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- pkgs/by-name/ma/matrix-synapse-unwrapped/package.nix
+++ pkgs/by-name/ma/matrix-synapse-unwrapped/package.nix
@@ -181,11 +181,11 @@ python3.pkgs.buildPythonApplication rec {
     updateScript = nix-update-script { };
   };
 
-  meta = with lib; {
+  meta = {
     homepage = "https://matrix.org";
     changelog = "https://github.com/element-hq/synapse/releases/tag/v${version}";
     description = "Matrix reference homeserver";
-    license = licenses.agpl3Plus;
-    maintainers = with lib.maintainers; teams.matrix.members ++ [ sumnerevans ];
+    license = lib.licenses.agpl3Plus;
+    maintainers = lib.teams.matrix.members ++ [ lib.maintainers.sumnerevans ];
   };
 }
