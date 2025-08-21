068e728f17904921fe0a99262e01b6575c2685c4
Nick Cao <nickcao@nichi.co>
2025-08-20 21:26:25 -0400

    matrix-authentication-service: apply qol patches

--- pkgs/by-name/ma/matrix-authentication-service/package.nix
+++ pkgs/by-name/ma/matrix-authentication-service/package.nix
@@ -2,6 +2,7 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  fetchpatch,
   fetchNpmDeps,
   npmHooks,
   nodejs,
@@ -28,6 +29,14 @@ rustPlatform.buildRustPackage (finalAttrs: {
   };
 
   cargoHash = "sha256-3ODOJY8/hksbMak8SsQP87rOnCd7+1G0wD4m5daPrbk=";
+
+  patches = [
+    # Add upstream_oauth2.providers.[].client_secret_file config option
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/element-hq/matrix-authentication-service/pull/4882.patch";
+      hash = "sha256-f4VN0gS1G5ahMwa8bMBTrOTtHPi7Ic5476zZgdCsx8A=";
+    })
+  ];
 
   npmDeps = fetchNpmDeps {
     name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
