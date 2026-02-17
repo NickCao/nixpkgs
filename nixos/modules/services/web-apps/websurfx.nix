7c84171a6fa8639e185bec2dc4ba72216b2a5e9f
Nick Cao <nickcao@nichi.co>
2026-02-16 20:24:29 -0500

    nixos/websurfx: adopt rfc42

--- nixos/modules/services/web-apps/websurfx.nix
+++ nixos/modules/services/web-apps/websurfx.nix
@@ -7,46 +7,8 @@
 
 let
   cfg = config.services.websurfx;
-  user = "websurfx";
-  dataDir = "/var/lib/${user}";
-
-  defaultConfig = {
-    logging = true;
-    debug = false;
-    threads = 10;
-    port = "8080";
-    binding_ip = "127.0.0.1";
-    production_use = false;
-    request_timeout = 30;
-    tcp_connection_keep_alive = 30;
-    pool_idle_connection_timeout = 30;
-    rate_limiter = {
-      number_of_requests = 20;
-      time_limit = 3;
-    };
-    https_adaptive_window_size = true;
-    operating_system_tls_certificates = true;
-    number_of_https_connections = 10;
-    client_connection_keep_alive = 120;
-    safe_search = 2;
-    colorscheme = "catppuccin-mocha";
-    theme = "simple";
-    animation = "simple-frosted-glow";
-    redis_url = "redis://127.0.0.1:8082";
-    cache_expiry_time = 600;
-    upstream_search_engines = {
-      DuckDuckGo = true;
-      Searx = false;
-      Brave = false;
-      Startpage = false;
-      LibreX = false;
-      Mojeek = false;
-      Bing = false;
-      Wikipedia = true;
-      Yahoo = false;
-    };
-    proxy = null;
-  };
+  settingsFormat = pkgs.formats.lua { asBindings = true; };
+  settingsFile = settingsFormat.generate "config.lua" cfg.settings;
 in
 {
   options = {
@@ -54,72 +16,106 @@ in
       enable = lib.mkEnableOption "Websurfx, a metasearch engine";
       package = lib.mkPackageOption pkgs "websurfx" { };
       openFirewall = lib.mkEnableOption "Whether to open the used port in the firewall";
-      redis = {
-        enable = lib.mkEnableOption "Websurfx analytics via Redis" // {
-          default = true;
-        };
-        port = lib.mkOption {
-          default = 4568;
-          description = "Websurfx Redis port";
-          type = lib.types.port;
-        };
+      redis.createLocally = lib.mkEnableOption "Configure local redis server for websurfx" // {
+        default = true;
       };
       settings = lib.mkOption {
-        default = {
-          port = "4567";
-          binding_ip = "127.0.0.1";
+        type = lib.types.submodule {
+          freeformType = settingsFormat.type;
+
+          options.binding_ip = lib.mkOption {
+            type = lib.types.str;
+            default = "127.0.0.1";
+            description = ''
+              ip address on the which server should be launched
+            '';
+          };
+          options.port = lib.mkOption {
+            type = lib.types.port;
+            default = 4567;
+            description = ''
+              port on which server should be launched
+            '';
+          };
         };
+        default = { };
         description = ''
           Configuration options for Websurfx, see
-          <https://github.com/neon-mmd/websurfx/blob/rolling/websurfx/config.lua>
+          [websurfx/config.lua](https://github.com/neon-mmd/websurfx/blob/rolling/websurfx/config.lua)
+          for supported values.
         '';
-        type = lib.types.attrs;
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = lib.optional cfg.openFirewall cfg.settings.port;
-    services = {
-      redis.servers.websurfx =
-        assert lib.assertMsg (
-          !cfg.redis.enable || (cfg.redis.port != cfg.settings.port)
-        ) "The websurfx redis and settings port need to differ.";
-        {
-          inherit (cfg.redis) enable port;
-        };
+    services.websurfx.settings = {
+      logging = lib.mkDefault true;
+      debug = lib.mkDefault false;
+      threads = lib.mkDefault 10;
+
+      production_use = lib.mkDefault false;
+
+      request_timeout = lib.mkDefault 30;
+      tcp_connection_keep_alive = lib.mkDefault 30;
+      pool_idle_connection_timeout = lib.mkDefault 30;
+      rate_limiter = {
+        number_of_requests = lib.mkDefault 20;
+        time_limit = lib.mkDefault 3;
+      };
+      https_adaptive_window_size = lib.mkDefault true;
+      operating_system_tls_certificates = lib.mkDefault true;
+      number_of_https_connections = lib.mkDefault 10;
+      client_connection_keep_alive = lib.mkDefault 120;
+      safe_search = lib.mkDefault 2;
+
+      colorscheme = lib.mkDefault "catppuccin-mocha";
+      theme = lib.mkDefault "simple";
+      animation = lib.mkDefault "simple-frosted-glow";
+
+      redis_url = lib.mkIf cfg.redis.createLocally "redis://${config.services.redis.servers.websurfx.unixSocket}";
+      cache_expiry_time = lib.mkDefault 600;
+
+      upstream_search_engines = {
+        DuckDuckGo = lib.mkDefault true;
+        Searx = lib.mkDefault false;
+        Brave = lib.mkDefault false;
+        Startpage = lib.mkDefault false;
+        LibreX = lib.mkDefault false;
+        Mojeek = lib.mkDefault false;
+        Bing = lib.mkDefault false;
+        Wikipedia = lib.mkDefault true;
+        Yahoo = lib.mkDefault false;
+      };
+
+      proxy = lib.mkDefault null;
     };
+
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.settings.port ];
+
+    services.redis.servers = lib.mkIf cfg.redis.createLocally {
+      websurfx.enable = true;
+    };
+
     systemd.services.websurfx = {
-      after = [ "network.target" ];
       description = "Websurfx, a metasearch engine";
-      environment.HOME = dataDir;
-      preStart = ''
-        mkdir -p ".config/websurfx"
-        cat '${
-          (pkgs.formats.lua { asBindings = true; }).generate "config.lua" (
-            defaultConfig
-            // {
-              redis_url = "redis://127.0.0.1:${toString cfg.redis.port}";
-            }
-            // cfg.settings
-          )
-        }' > ".config/websurfx/config.lua"
-      '';
-      script = lib.getExe cfg.package;
+
       serviceConfig = {
-        User = user;
-        WorkingDirectory = dataDir;
+        ExecStart = lib.getExe cfg.package;
+        DynamicUser = true;
+        SupplementaryGroups = lib.mkIf cfg.redis.createLocally [
+          config.services.redis.servers.websurfx.group
+        ];
+        RuntimeDirectory = "websurfx";
+        Environment = [ "HOME=%t/websurfx" ];
+        BindReadOnlyPaths = [
+          "${settingsFile}:%t/websurfx/.config/websurfx/config.lua"
+        ];
       };
+
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
-    };
-    users = {
-      groups.${user} = { };
-      users.${user} = {
-        createHome = true;
-        group = user;
-        home = dataDir;
-        isSystemUser = true;
-      };
     };
   };
 
